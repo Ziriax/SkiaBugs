d1779e809acbe5a478213fb4053cfd9cefc6f751
Peter Verswyvelen <bugfact@gmail.com>
2020-02-07 14:00:49 +0100

    NEW: First version showing text bug when running in Docker

---
+++ DockerTextBug/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using SkiaSharp;
+
+namespace DockerTextBug
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Rendering DockerTextBug...");
+
+			using var surface = SKSurface.Create(new SKImageInfo(256, 256));
+			using var canvas = surface.Canvas;
+			canvas.Clear(SKColors.SkyBlue);
+			using var paint = new SKPaint { Color = SKColors.Yellow, TextAlign = SKTextAlign.Center, TextSize = 64 };
+			canvas.DrawText("foobar", 128, 128, paint);
+			using var image = surface.Snapshot();
+			using var data = image.Encode();
+
+			using var stream = File.Create($"test_{Environment.OSVersion.Platform}.png");
+			Console.WriteLine($"Saving to {stream.Name}...");
+			data.SaveTo(stream);
+		}
+	}
+}
